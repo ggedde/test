0bfeeb3439c421fbfdc8a425ea9ea87fe4e50417
GGedde <ggedde@users.noreply.github.com>
2020-06-01 13:38:39 -0700

    Update index.php

--- index.php
+++ index.php
@@ -7,5 +7,7 @@ echo '<pre>'; print_r($_SERVER); echo '</pre>';
 echo '<pre>'; print_r($_ENV); echo '</pre>';
 
 echo getenv('TEST');
+echo getenv('DOCUMENT_ROOT');
+echo getenv('APP_BRANCH');
 
 phpinfo();
